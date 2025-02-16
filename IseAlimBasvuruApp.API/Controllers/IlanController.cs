0a1e1bd31ce6bb0aff7fed9f84378921c5b4cd71
Okan Topdemir <149389691+OkanT6@users.noreply.github.com>
2025-02-16 22:19:59 +0300

    Add files via upload

---
+++ IseAlimBasvuruApp.API/Controllers/IlanController.cs
@@ -0,0 +1,90 @@
+﻿//using IseAlimBasvuruApp.Application.DTOs;
+using IseAlimBasvuruApp.Application.Services.Interfaces;
+using IseAlimBasvuruApp.Domain.DTOs;
+using IseAlimBasvuruApp.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace IseAlimBasvuruApp.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class IlanController : ControllerBase
+    {
+        private readonly IKullaniciService _kullaniciService;
+        private readonly IIlanService _ilanService;
+
+        public IlanController(IKullaniciService kullaniciService, IIlanService ilanService)
+        {
+            _kullaniciService = kullaniciService;
+            _ilanService = ilanService;
+        }
+
+        
+        [HttpGet]
+        public async Task<IlanYayinDetayDTO> IlanDetayGoruntule([FromQuery] int id)
+        {
+            return await _ilanService.IlanDetayGoruntule(id);   
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<IlanYayinDTO>> ilanlariListeleAktif()
+        {
+        
+
+            return await _ilanService.ilanlariListeleAktif();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Yönetici")]
+        public IActionResult ilanYayinla(IlanDTO ilanDTO) {
+
+            //Tokenden ID değerine ulaşıyorum
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (string.IsNullOrEmpty(userIdClaim))
+                return Unauthorized("Kimlik doğrulaması yapılırken hata oluştu.");
+
+            // Token'daki ID'yi int'e dönüştür
+            if (!int.TryParse(userIdClaim, out var id))
+                return BadRequest("Token'daki kullanıcı ID'si geçerli bir sayı değil.");
+
+            var yoneticiId = id;
+
+            Ilan ilan =_ilanService.IlanOlusturAsync(ilanDTO, yoneticiId);
+            if(ilan!=null)
+            {
+                return Ok("Ilan basarili bir sekilde eklenmistir");
+            }
+            else
+            {
+                return Ok("Ilan eklerken bir sorunla karşılaşıldı");
+            }
+        }
+
+        [HttpPut]
+        [Authorize(Roles = "Yönetici")]
+        public async Task<IActionResult> IlaniKapat(int ilanId)
+        {
+            //Belki ileride her yönetici sadece kendi ilanını kapatbilsin şartı koşabilirim
+            //Şu anda böyle bir şart yok!
+
+            var ilan = await _ilanService.IlanGetirAsync(ilanId); 
+            if (ilan == null)
+            {
+                return NotFound(new { message = "İlan bulunamadı." });
+            }
+
+            await _ilanService.KapatIlanAsync(ilanId);
+
+            return Ok(new { message = "İlan başarıyla kapatıldı." });
+        }
+
+
+
+
+    }
+
+    
+}
---
+++ IseAlimBasvuruApp.API/Controllers/KullaniciController.cs
@@ -0,0 +1,96 @@
+﻿using IseAlimBasvuruApp.Application.DTOs;
+using IseAlimBasvuruApp.Domain.DTOs;
+using IseAlimBasvuruApp.Application.Services.Implementations;
+using IseAlimBasvuruApp.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace IseAlimBasvuruApp.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class KullaniciController : ControllerBase
+    {
+        private readonly IKullaniciService _kullaniciService;
+        private readonly ICloudinaryService _cloudinaryService;   
+
+        public KullaniciController(IKullaniciService kullaniciService,ICloudinaryService cloudinaryService)
+        {
+            _kullaniciService = kullaniciService;
+            _cloudinaryService = cloudinaryService;
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Komisyon,Yönetici")]
+        public IActionResult getKullanici([FromQuery] int id)
+        {
+            var result = _kullaniciService.KullaniciGetir(id);
+            if (result == null) 
+                return NotFound("Böyle bir kullanıcı yoktur");
+            else
+            return Ok(result); // Sadece `result` JSON olarak dönecek!
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> kayitOl([FromBody] Domain.DTOs.KayitDTO kayitDTO)
+        {
+            var sonuc = await _kullaniciService.KayitOl(kayitDTO); // await ekledik!
+            return Ok(sonuc);
+        }
+
+
+        [HttpPost]
+        public IActionResult girisYap([FromBody] GirisDTO girisDTO)
+        {
+
+            var sonuc = _kullaniciService.GirisYap(girisDTO);
+            return Ok(sonuc);
+        }
+        [HttpGet]
+        [Authorize(Roles = "Aday")]
+        public IActionResult profilBilgileriniGoruntule() {
+
+            //Tokenden ID değerine ulaşıyorum
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (string.IsNullOrEmpty(userIdClaim))
+                return Unauthorized("Kimlik doğrulaması yapılırken hata oluştu.");
+
+            // Token'daki ID'yi int'e dönüştür
+            if (!int.TryParse(userIdClaim, out var id))
+                return BadRequest("Token'daki kullanıcı ID'si geçerli bir sayı değil.");
+
+            var kullaniciId = id;
+
+            var sonuc=_kullaniciService.KullaniciGetir(kullaniciId);
+            return Ok(sonuc);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Aday")]
+        public IActionResult profilFotografiniDegistir([FromQuery] IFormFile file)
+        {
+            //Tokenden ID değerine ulaşıyorum
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (string.IsNullOrEmpty(userIdClaim))
+                return Unauthorized("Kimlik doğrulaması yapılırken hata oluştu.");
+
+            // Token'daki ID'yi int'e dönüştür
+            if (!int.TryParse(userIdClaim, out var id))
+                return BadRequest("Token'daki kullanıcı ID'si geçerli bir sayı değil.");
+
+            var kullaniciId = id;
+
+           var istekSonucu= _cloudinaryService.UploadProfilFoto(file, kullaniciId);
+
+            if (istekSonucu.statusCode == 200)
+                return Ok(istekSonucu.message);
+            else
+                return BadRequest("Hata Kodu:" + istekSonucu.message + " " +istekSonucu.statusCode );
+
+        }
+
+
+    }
+}
